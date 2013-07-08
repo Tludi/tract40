13e125534415a0793b9492a2d65fb1512a4218b1
Tludi <tludi00@gmail.com>
2013-07-08 12:13:48 -0700

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Tract40::Application.initialize!
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tract40::Application.config.session_store :cookie_store, key: '_tract40_session'
